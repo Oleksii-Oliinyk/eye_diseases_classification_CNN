7ddafcb33461b8a571ac6a67767f9133c0c80abd
Oleksii <oleksii.oliinyk.work@gmail.com>
2025-04-28 23:36:00 +0300

    fix validation data split for classication model

--- classification_model/model.py
+++ classification_model/model.py
@@ -12,14 +12,14 @@ class EyeDiseaseClassifierCNN(nn.Module):
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
         self.conv3_bn = nn.BatchNorm2d(32)
         
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.15)
+        self.dropout1 = nn.Dropout(0.35)
+        #self.dropout2 = nn.Dropout(0.15)
         
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.fc1 = nn.Linear(64 * 64 * 32, 2048)
-        self.fc2 = nn.Linear(2048, 256)
-        self.fc3 = nn.Linear(256, 8)
+        self.fc1 = nn.Linear(80 * 80 * 32, 1024)
+        self.fc2 = nn.Linear(1024, 8)
+        #self.fc3 = nn.Linear(256, 8)
         
     def forward(self, x):
         x = self.conv1(x)
@@ -43,11 +43,7 @@ class EyeDiseaseClassifierCNN(nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        
-        x = self.fc3(x)
+        x = self.fc2(x)       
         return x
         
         
