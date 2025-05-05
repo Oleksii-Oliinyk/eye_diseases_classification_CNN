c15fdf5794d2826b555ef9a934efe80df85cbfd9
Oleksii <oleksii.oliinyk.work@gmail.com>
2025-05-05 14:37:16 +0300

    add augmentation for retinoblastoma dataset and add training loop

--- retinoblastoma_model/model.py
+++ retinoblastoma_model/model.py
@@ -1,6 +1,6 @@
-import torch
 import torch.nn.functional as F
 import torch.nn as nn
+from torch import flatten
 
 class RetinoblastomaClassifierCNN(nn.Module):
     def __init__(self):
@@ -16,8 +16,8 @@ class RetinoblastomaClassifierCNN(nn.Module):
         
         self.dropout = nn.Dropout(0.3)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 16 * 16, 256)
-        self.fc2 = nn.Linear(256, 1)
+        self.fc1 = nn.Linear(64 * 16 * 16, 128)
+        self.fc2 = nn.Linear(128, 1)
         
     def forward(self, x):
         x = self.conv1(x)
@@ -40,8 +40,8 @@ class RetinoblastomaClassifierCNN(nn.Module):
         x = F.relu(x)
         x = self.pool(x)
         
-        x = torch.flatten(x, start_dim=1)
+        x = flatten(x, start_dim=1)
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
-        x = F.sigmoid(self.fc2(x))
+        x = self.fc2(x)
         return x
